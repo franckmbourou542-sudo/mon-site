23a4360730a643b81acca89a13fd1267bdd4df9f
franckmbourou542-sudo <franckmbourou542@gmail.com>
2026-05-28 08:18:14 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+"nom-de-l-id": {
+    titre: "Nom de l'anime affiché",
+    image: "images/votre-image.jpg",
+    badge: "Saison 1",
+    episodes: [
+        { id: "01", fichier: "videos/video1.mp4", titre: "Titre de l'épisode 1" },
+        { id: "02", fichier: "videos/video2.mp4", titre: "Titre de l'épisode 2" }
+    ]
+}
